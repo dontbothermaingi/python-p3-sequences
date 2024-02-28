4db2a9ca476dc44aa1ca8337afd393ca726b4108
+ <+>
2024-02-28 11:22:25 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    if length == 0:
+        print("empty list")
+        
+    elif length == 1:
+        fibonacci_sequence.append(0)
+    else:
+        fibonacci_sequence = [0, 1]
+        while len(fibonacci_sequence) < length:
+            next_num = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_num)
+    print(fibonacci_sequence)
+    return fibonacci_sequence
